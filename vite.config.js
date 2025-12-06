a186c4ad58329042e9b7a0064b10ffdcfb150312
Richard M <36093669+itsrichardmai@users.noreply.github.com>
2025-12-05 22:45:20 -0800

    updated vite.config
    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/DubaKaraokePub2/' : '/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
